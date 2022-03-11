d03b6ecde816395e85e8dceaca089632305fe201
caique almeida <caaguima@gmail.com>
2022-03-11 16:45:12 -0300

    fix: button font styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,12 +2,15 @@ import styled, { css } from 'styled-components'
 
 export const Button = styled.button`
   ${({ theme }) => css`
-    padding: ${theme.spacing.xxxs} ${theme.spacing.xxl};
+    padding: 14.5px 75.5px;
     border-radius: ${theme.borders.radius.lg};
     background: ${theme.colors.gradient};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     transition: all 0.3s ease-in-out;
     transform: translate(0 0);
+
+    font-size: ${theme.font.size.sm};
+    font-weight: ${theme.font.weight.semibold};
 
     &:hover {
       filter: brightness(0.85);
