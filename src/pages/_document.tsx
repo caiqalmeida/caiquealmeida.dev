c8e9268d8dbe0a050ab547d6ce99819cf0974452
caique almeida <caaguima@gmail.com>
2022-03-24 17:58:53 -0300

    feat: added font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,17 @@
-import Document, { DocumentInitialProps, DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -33,14 +42,22 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" type="image/x-icon" href="/logo.svg" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import Head from 'next/head'
+import styled from 'styled-components'
+
+import { SITE_TITLE } from '../constants/head'
+import Button from '../components/Button'
+import Navbar from '../components/Navbar'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+  color: ${({ theme }) => theme.colors.white.light};
+`
 
 const Home: React.FC = () => {
-  return <Title>Hello World</Title>;
-};
+  return (
+    <>
+      <Head>
+        <title>{SITE_TITLE} | home</title>
+      </Head>
+      <Title>Hello World !</Title>
+      <Button>Botão</Button>
+      <Navbar />
+    </>
+  )
+}
 
-export default Home;
+export default Home
